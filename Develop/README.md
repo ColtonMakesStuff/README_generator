bdd0db7e7d02daef3aa4cbd878e0e98907d072ee
Colton Firestone <colton.firestone@gmail.com>
2023-08-16 23:49:04 -0700

    added most functionallity, still needs video and other details

---
+++ Develop/README.md
@@ -0,0 +1,35 @@
+
+
+# README Generator
+
+## Introduction
+
+a command-line application that dynamically generates a professional README.md file from a user's input
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+## Installation
+clone repository
+
+## Usage
+The application will be invoked by using the following command: node index.js
+
+## License
+none
+
+
+## Contributing
+
+Contributions are not allowed at this time Please follow the guidelines.
+
+## Video Link 
+
+## repository link 
+
+
+  
--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown')
+
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -36,9 +36,9 @@ const questions = [
         message: 'Please provide any testing instructions:'
       },
       {
-        name: 'github_link',
+        name: 'github_username',
         type: 'input',
-        message: 'Please enter your GitHub profile link:'
+        message: 'Please enter your GitHub Username:'
       },
       {
         name: 'email',
@@ -49,26 +49,42 @@ const questions = [
         name: 'selected_license',
         type: 'list',
         message: 'Select a license:',
-        choices: ['Option 1', 'Option 2', 'Option 3']
+        choices: ['GPL', 'CC BY', 'MIT']
       }
 ];
 
 // TODO: Create a function to write README file
-function writeREADME(readmeContent) {
-    fs.writeFile('./utils/README.md', readmeContent, function (err) {
-      if (err) throw err;
-      console.log('README file created successfully.');
-    });
+function writeREADME(readmeContent, answers) {
+  const userDirectory = `./userREADME/${answers.github_username}`;
+
+
+  if (!fs.existsSync(userDirectory)) {
+    fs.mkdirSync(userDirectory, { recursive: true });
   }
 
+  fs.writeFile(`${userDirectory}/README.md`, readmeContent, function (err) {
+    if (err) throw err;
+    console.log('README file created successfully.');
+  });
+}
 
 
 
-// Function to prompt the next question
+
+// Function to ask the next question
 function promptNextQuestion(index, answers) {
     if (index === questions.length) {
       // All questions have been answered, generate the README content and write the file
       console.log('All questions answered:', answers);
+
+//define the proper license
+      if (answers.selected_license === 'GPL') {
+        var license_Type = 'A copyleft license that ensures the freedom to use, modify, and distribute software while requiring any derivative work or software using GPL-licensed code to be released under the GPL as well. [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      } else if (answers.selected_license === 'CC BY') {
+        var license_Type = 'CC BY License: A Creative Commons license that permits others to distribute, remix, adapt, and build upon the licensed work, even for commercial purposes, as long as attribution is given to the original creator. [![License: CC BY 4.0](https://licensebuttons.net/l/by/4.0/80x15.png)](https://creativecommons.org/licenses/by/4.0/)' ;
+      } else if (answers.selected_license === 'MIT') {
+        var license_Type = 'A permissive open-source license that allows users to reuse and modify the code for any purpose, including in proprietary software, as long as the original license and copyright notice are included. [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      } 
 
       const readmeContent = `
 
@@ -86,41 +102,42 @@ ${answers.project_description}
 - [License](#license)
 
 ## Installation
-...
+${answers.install_instruct}
 
 ## Usage
-...
+${answers.usage}
+
 
 ## License
-...
 
-## Installation
+${license_Type}
 
-Use the following command to install the project dependencies:
-
-\`\`\`
-npm install
-\`\`\`
-
-## Usage
-
-To run the project, use the following command:
-
-\`\`\`
-npm start
-\`\`\`
 
 ## Contributing
 
 Contributions are welcome! Please follow the guidelines.
 
+${answers.contributing}
+
 ## License
 
-This project is licensed under the MIT License.
+This project is licensed under the ${answers.license}
+License.
 
-[Visit my website](https://www.example.com "My Website")
+##Tests
+
+${answers.tests}
+
+
+[Visit my website](https://www.${answers.github_username}
+.com "My Website")
+
+## Contact me with any questions via email 
+
+${answers.email}
+
   `;
-      writeREADME(readmeContent);
+      writeREADME(readmeContent, answers);
     } else {
       // Prompt the next question
       inquirer.prompt([questions[index]]).then(answer => {
